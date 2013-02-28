5eccdc3b1bbaea08888c02ed675475280dfbb06d
prideout <philiprideout@gmail.com>
2013-02-27 20:37:07 -0800

    more changes to be amenable to superproject

---
+++ demos/demolist.js
@@ -0,0 +1,9 @@
+var demolist = {
+  ColorWheel: "Color Wheel",
+  ParametricSurf: "Parametric Surface",
+  BasicLighting: "Basic Lighting",
+  FacetNormals: "Facet Normals",
+  SmoothNormals: "Smooth Normals",
+  PolygonTess: "Polygon Tessellation",
+  ThumbTest: "Thumb Test",
+};
