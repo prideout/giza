3f7256f6d8e8463a817596504ff2ffaec9e490ab
prideout <philiprideout@gmail.com>
2013-03-02 17:21:58 -0800

    cleanup

--- giza/Shaders.js
+++ giza/Shaders.js
@@ -3,21 +3,19 @@ var GIZA = GIZA || {};
 // Use the supplied JSON metadata to fetch GLSL, compile it, bind
 // attributes, and finally link it into a program object.
 GIZA.compile = function(shaders) {
-  var name, programs, shd;
-  programs = {};
-  for (name in shaders) {
-    shd = shaders[name];
+  var programs = {};
+  for (var name in shaders) {
+    var shd = shaders[name];
     programs[name] = GIZA.compileProgram(shd.vs, shd.fs, shd.attribs);
   }
   return programs;
 };
 
 GIZA.compileProgram = function(vNames, fNames, attribs) {
-  var fShader, key, numUniforms, program, status, u, uniforms, vShader, value, _i, _len;
   var gl = GIZA.context;
-  vShader = GIZA.compileShader(vNames, gl.VERTEX_SHADER);
-  fShader = GIZA.compileShader(fNames, gl.FRAGMENT_SHADER);
-  program = gl.createProgram();
+  var vShader = GIZA.compileShader(vNames, gl.VERTEX_SHADER);
+  var fShader = GIZA.compileShader(fNames, gl.FRAGMENT_SHADER);
+  var program = gl.createProgram();
   gl.attachShader(program, vShader);
   gl.attachShader(program, fShader);
   for (key in attribs) {
@@ -25,22 +23,14 @@ GIZA.compileProgram = function(vNames, fNames, attribs) {
     gl.bindAttribLocation(program, value, key);
   }
   gl.linkProgram(program);
-  status = gl.getProgramParameter(program, gl.LINK_STATUS);
+  var status = gl.getProgramParameter(program, gl.LINK_STATUS);
   if (!status) {
     console.error("Could not link " + vNames + " with " + fNames);
   }
-  numUniforms = gl.getProgramParameter(program, gl.ACTIVE_UNIFORMS);
-  uniforms = (function() {
-    var _i, _results;
-    _results = [];
-    for (u = _i = 0; 0 <= numUniforms ? _i < numUniforms : _i > numUniforms; u = 0 <= numUniforms ? ++_i : --_i) {
-      _results.push(gl.getActiveUniform(program, u).name);
-    }
-    return _results;
-  })();
-  for (_i = 0, _len = uniforms.length; _i < _len; _i++) {
-    u = uniforms[_i];
-    program[u] = gl.getUniformLocation(program, u);
+  var numUniforms = gl.getProgramParameter(program, gl.ACTIVE_UNIFORMS);
+  for (var i = 0; i < numUniforms; i++) {
+    var name = gl.getActiveUniform(program, i).name;
+    program[name] = gl.getUniformLocation(program, name);
   }
   return program;
 };
@@ -59,7 +49,7 @@ GIZA.compileShader = function(ids, type) {
   var shaderObject = gl.createShader(type);
   gl.shaderSource(shaderObject, sourceText);
   gl.compileShader(shaderObject);
-  status = gl.getShaderParameter(shaderObject, gl.COMPILE_STATUS);
+  var status = gl.getShaderParameter(shaderObject, gl.COMPILE_STATUS);
   if (!status) {
     console.error('GLSL error in ' + ids + ':\n' +
                   gl.getShaderInfoLog(shaderObject));
