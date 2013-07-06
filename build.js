45a028dfd2e66681d7b928681e968c3abf9c407a
Philip Rideout <philiprideout@gmail.com>
2013-07-05 19:36:34 -0700

    restore build.js

---
+++ build.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+
+var gizapath = "./giza/";
+var scripts = [
+  gizapath + "Giza.js",
+  gizapath + "Utility.js",
+  gizapath + "Animation.js",
+  gizapath + "Shaders.js",
+  gizapath + "BufferView.js",
+  gizapath + "Vector.js",
+  gizapath + "Matrix.js",
+  gizapath + "Color.js",
+  gizapath + "Topo.js",
+  gizapath + "Polygon.js",
+  gizapath + "Surface.js",
+  gizapath + "Path.js",
+  gizapath + "Mouse.js",
+  gizapath + "Turntable.js",
+  gizapath + "Fullscreen.js",
+];
+
+var UglifyJS = require("uglify-js");
+var fs = require("fs");
+var result = UglifyJS.minify(scripts);
+fs.writeFileSync('giza.min.js', result.code);
